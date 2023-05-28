51a1a9259c718fdb443770ec901d3d449193af97
4 TA <aminechallali@hotmail.fr>
2023-05-28 20:16:08 +0200

    New commit

--- src/routes/RoutesPath.js
+++ src/routes/RoutesPath.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import Accueil from "../pages/Accueil/Accueil";
 import Scan from "../pages/Scan/Scan";
@@ -11,7 +11,8 @@ function RoutesPath() {
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<Accueil />} />
+          <Route path="/" element={<Navigate to="/accueil" />} /> {/* Redirection vers la page d'accueil */}
+          <Route path="/accueil" element={<Accueil />} /> {/* Page d'accueil */}
           <Route path="/scans/:scan" element={<Scan />} />
           <Route path="/recherche/:searchTerm" element={<Recherche />} />
           <Route path="*" element={<NotFound />} /> {/* Route pour toutes les URL non connues */}
