640cc857d65f9c84395f0dedf376f654af3207ab
4 TA <aminechallali@hotmail.fr>
2023-05-28 20:24:43 +0200

    New commit

--- src/routes/RoutesPath.js
+++ src/routes/RoutesPath.js
@@ -1,24 +1,23 @@
 import React from "react";
-import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import Accueil from "../pages/Accueil/Accueil";
 import Scan from "../pages/Scan/Scan";
 import Recherche from "../pages/Recherche/Recherche";
-import NotFound from "../pages/NotFound/NotFound"; // Import du composant NotFound
+import NotFound from "../pages/NotFound/NotFound";
 
 function RoutesPath() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<Navigate to="/accueil" />} /> {/* Redirection vers la page d'accueil */}
-          <Route path="/accueil" element={<Accueil />} /> {/* Page d'accueil */}
+          <Route path="/" element={<Accueil />} />
           <Route path="/scans/:scan" element={<Scan />} />
           <Route path="/recherche/:searchTerm" element={<Recherche />} />
-          <Route path="*" element={<NotFound />} /> {/* Route pour toutes les URL non connues */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
